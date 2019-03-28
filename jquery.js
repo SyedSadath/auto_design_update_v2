0937949e6c667f37de6adf6a7d6a66f4049805e1
root <root@Mr.bot>
2019-03-28 13:38:17 +0530

    First commit

--- jquery.js
+++ jquery.js
@@ -4,15 +4,19 @@ $(document).ready(function(){
 	$('#particles-js').hover(function(){
 
 		$('.mimg').attr('src','img/wheel.png');
+		$('.banner34').attr('src','img/car.png');
 	});
 
+
+
 	$('.banner34').hover(function(){
-		$('.mimg').attr('src','img/wheel.png');
+		$(this).attr('src','img/mustang_car.png');
+		$('.mimg').attr('src', 'img/wheel_new.png');
 	});
 
 	$('.mimg').hover(function(){
 		$(this).attr('src', 'img/wheel_new.png');
-		$(this).attr("transform","scale(1.2,1.2)")
+		$('.banner34').attr('src','img/mustang_car.png');
 		
 	});
 		
